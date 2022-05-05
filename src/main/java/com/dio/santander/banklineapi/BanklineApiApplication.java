2c6ee0b3a4a46fe6817a7dbfd767cfd110700b66
TiagoMata <56051094+TiagoMata@users.noreply.github.com>
2022-05-04 22:02:42 -0300

    alterando o arquivo principal

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -8,6 +8,9 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		// vamos começar
+		
+		System.out.print("ola");
 	}
 
 }
